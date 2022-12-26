f34db940815510a05daeb1d18b66e868c66c0d38
Mathias <mathias.andre@epitech.eu>
2022-12-26 18:18:19 +0100

    Client folder

--- mango.py
+++ mango.py
@@ -1,12 +1,17 @@
 from command import MangoCommand
 from server.example import pull
+from client.tests import run_test
 import sys
 
 commands = {
-    MangoCommand("pull", [], pull)
+    MangoCommand("pull", [], pull),
+    MangoCommand("test", ["name"], run_test)
 }
 
 for command in commands:
     if command.name == sys.argv[1]:
-        command.action()
+        if len(sys.argv) - 2 == len(command.args):
+            command.action(*sys.argv[2:])
+        else:
+            print(f'Usage: mango {command.name} {" ".join([f"<{arg}>" for arg in command.args])}')
         break
